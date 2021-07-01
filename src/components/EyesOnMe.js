d22d1d92e6a1f6789857a3235458fee1391a1cc3
Hadi Abaza <Hadiabaza1@gmail.com>
2021-06-30 20:34:19 -0400

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+function EyesOnMe() {
+  function FocusHandler() {
+    console.log("Good!");
+  }
+  function BlurHandler() {
+    console.log("Hey! Eyes on me!");
+  }
+  return (
+    <button onFocus={FocusHandler} onBlur={BlurHandler}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
 // Code Keypad Component Here
+function Keypad() {
+  function eventHandler() {
+    console.log("Entering password...");
+  }
+  return <input type="password" onChange={eventHandler} />;
+}
+
+export default Keypad;
